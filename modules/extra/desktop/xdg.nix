9672acb5dfce17237cb1defc1ff932a30d2ae2d5
Axioms <atomic.axioms@gmail.com>
2025-11-28 20:30:53 -0500

    fix mime

--- modules/extra/desktop/xdg.nix
+++ modules/extra/desktop/xdg.nix
@@ -11,42 +11,16 @@
   # Portals
   home-manager.users.axiom = {
     xdg = {
-      mimeApps =
-        let
-          value =
-            let
-              zen-browser = inputs.zen-browser.packages.${system}.beta; # or twilight
-            in
-            zen-browser.meta.desktopFileName;
-
-          associations = builtins.listToAttrs (
-            map
-              (name: {
-                inherit name value;
-              })
-              [
-                "application/x-extension-shtml"
-                "application/x-extension-xhtml"
-                "application/x-extension-html"
-                "application/x-extension-xht"
-                "application/x-extension-htm"
-                "x-scheme-handler/unknown"
-                "x-scheme-handler/mailto"
-                "x-scheme-handler/chrome"
-                "x-scheme-handler/about"
-                "x-scheme-handler/https"
-                "x-scheme-handler/http"
-                "application/xhtml+xml"
-                "application/json"
-                "text/plain"
-                "text/html"
-              ]
-          );
-        in
-        {
-          associations.added = associations;
-          defaultApplications = associations;
+      mimeApps = {
+        enable = true;
+        associations.added = {
+          "inode/directory" = [ "org.kde.dolphin.desktop" ];
         };
+
+        defaultApplications = {
+          "inode/directory" = [ "org.kde.dolphin.desktop" ];
+        };
+      };
 
       portal = {
         config = {
