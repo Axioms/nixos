c1f034ea4a2ccd36d0550a97cb81e67d818c2300
Axiom <axiom@axioms.dev>
2026-01-08 09:24:00 -0500

    add fingerprint pam config

--- modules/extra/fingerprint.nix
+++ modules/extra/fingerprint.nix
@@ -8,4 +8,12 @@
   ];
 
   services.fprintd.enable = true;
+
+  services.xserver.displayManager.sddm.extraConfig = ''
+  # Include the default SDDM PAM configuration
+  @include /etc/pam.d/sddm
+
+  # Add the fingerprint authentication module for login
+  auth        sufficient    pam_fprintd.so
+'';
 }
