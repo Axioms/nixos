742ab357e3f80aa75df656ce2386d3269163241b
Axioms <atomic.axioms@gmail.com>
2026-01-18 00:51:09 -0500

    update hyprland cursor

--- flake.nix
+++ flake.nix
@@ -4,7 +4,6 @@
     nixpkgs-streamcontroller.url = "github:NixOS/nixpkgs/pull/416567/head";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.11";
-    hyprland.url = "github:hyprwm/Hyprland";
     flake-utils.url = "github:numtide/flake-utils";
     agenix.url = "github:ryantm/agenix";
 
@@ -27,10 +26,6 @@
     home-manager-unstable = {
       url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs-unstable";
-    };
-    hypr-dynamic-cursors = {
-        url = "github:VirtCode/hypr-dynamic-cursors";
-        inputs.hyprland.follows = "hyprland";
     };
     lanzaboote = {
       url = "github:nix-community/lanzaboote/v1.0.0";
--- users/axiom/default.nix
+++ users/axiom/default.nix
@@ -20,7 +20,6 @@
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.axiom = {
     isNormalUser = true;
-
     shell = pkgs.zsh;
     description = "axiom";
     group = "axiom";
