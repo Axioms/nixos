baba7391716554b2403f89cbde63f747fae5a957
Axiom <axiom@axioms.dev>
2026-01-25 04:15:24 -0500

    plymouth updates

--- modules/extra/plymouth.nix
+++ modules/extra/plymouth.nix
@@ -1,29 +1,37 @@
-{ pkgs, ... }:
+{ pkgs, lib, config, ... }:
 {
-  boot = {
-
-    plymouth = {
-      enable = true;
-      theme = "pixels";
-      themePackages = with pkgs; [
-        # By default we would install all themes
-        (adi1090x-plymouth-themes.override {
-          selected_themes = [ "pixels" ];
-        })
-      ];
+  options = {
+    plymouth.theme = lib.mkOption {
+      type = lib.types.str;
+      default = "pixels";
     };
+  };
 
-    # Enable "Silent boot"
-    consoleLogLevel = 3;
-    initrd.verbose = false;
-    kernelParams = [
-      "quiet"
-      "udev.log_level=3"
-      "systemd.show_status=auto"
-    ];
-    # Hide the OS choice for bootloaders.
-    # It's still possible to open the bootloader list by pressing any key
-    # It will just not appear on screen unless a key is pressed
-    loader.timeout = 2;
+  config = {
+    boot = {
+      plymouth = {
+        enable = true;
+        theme = config.plymouth.theme;
+        themePackages = with pkgs; [
+          # By default we would install all themes
+          (adi1090x-plymouth-themes.override {
+            selected_themes = [ config.plymouth.theme ];
+          })
+        ];
+      };
+
+      # Enable "Silent boot"
+      consoleLogLevel = 3;
+      initrd.verbose = false;
+      kernelParams = [
+        "quiet"
+        "udev.log_level=3"
+        "systemd.show_status=auto"
+      ];
+      # Hide the OS choice for bootloaders.
+      # It's still possible to open the bootloader list by pressing any key
+      # It will just not appear on screen unless a key is pressed
+      loader.timeout = 2;
+    };
   };
 }
