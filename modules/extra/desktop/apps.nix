8d18c8f61b4bb5ed5fc2ccf06f4d3302edc99193
Axioms <atomic.axioms@gmail.com>
2026-01-31 23:52:03 -0500

    add apps

--- modules/extra/desktop/apps.nix
+++ modules/extra/desktop/apps.nix
@@ -3,6 +3,7 @@
 {
   environment.systemPackages = with pkgs; [
     birdtray
+    bitwarden-desktop
     deskflow
     jellyfin-media-player
     looking-glass-client
--- modules/extra/steam.nix
+++ modules/extra/steam.nix
@@ -1,15 +1,29 @@
 { pkgs, ... }:
 
 {
-  programs.steam = {
-    enable = true;
-    localNetworkGameTransfers.openFirewall = true;
-    dedicatedServer.openFirewall = true;
-    remotePlay.openFirewall = true;
+  programs = {
+    gamescope = {
+      enable = true;
+      capSysNice = true;
+    };
+    steam = {
+      enable = true;
+      localNetworkGameTransfers.openFirewall = true;
+      dedicatedServer.openFirewall = true;
+      remotePlay.openFirewall = true;
+      gamescopeSession.enable = true;
 
-    extraPackages = with pkgs; [
-      kdePackages.breeze-icons
-      kdePackages.breeze
-    ];
+      extraPackages = with pkgs; [
+        kdePackages.breeze-icons
+        kdePackages.breeze
+      ];
+    };
+
+    programs.appimage = {
+      enable = true;
+      binfmt = true;
+
+    };
   };
+  hardware.xone.enable = true; # support for the xbox controller USB dongle
 }
