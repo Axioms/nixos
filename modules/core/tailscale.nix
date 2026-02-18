36d5849a8637e9e16c87c0b40afef6cca1fdacc9
Axioms <axiom@axioms.dev>
2026-02-18 00:59:13 -0500

    add tailscale packages

--- modules/core/tailscale.nix
+++ modules/core/tailscale.nix
@@ -1,4 +1,4 @@
-{ inputs, ... }:
+{ pkgs, ... }:
 
 {
 
@@ -8,6 +8,11 @@
     extraSetFlags = [ "--netfilter-mode=nodivert" ];
     extraDaemonFlags = [ "--no-logs-no-support" ];
   };
+
+  environment.systemPackages = with pkgs; [
+    tail-tray
+    trayscale
+  ];
 
   #networking.firewall.checkReversePath = "loose";
 }
