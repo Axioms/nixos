a47b10d4be61f5e7a1473dbd383d04e28296dfe1
Axioms <atomic.axioms@gmail.com>
2026-03-13 22:39:43 -0400

    change zen default

--- modules/extra/browsers/zen.nix
+++ modules/extra/browsers/zen.nix
@@ -13,7 +13,7 @@
     ];
     programs.zen-browser = {
       enable = true;
-
+      setAsDefaultBrowser = true;
       policies = {
         AutofillAddressEnabled = false;
         AutofillCreditCardEnabled = false;
@@ -583,43 +583,5 @@
         };
       */
     };
-
-    xdg.mimeApps =
-      let
-        value =
-          let
-            zen-browser = inputs.zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.beta; # or twilight
-          in
-          zen-browser.meta.desktopFileName;
-
-        associations = builtins.listToAttrs (
-          map
-            (name: {
-              inherit name value;
-            })
-            [
-              "application/x-extension-shtml"
-              "application/x-extension-xhtml"
-              "application/x-extension-html"
-              "application/x-extension-xht"
-              "application/x-extension-htm"
-              "x-scheme-handler/unknown"
-              "x-scheme-handler/mailto"
-              "x-scheme-handler/chrome"
-              "x-scheme-handler/about"
-              "x-scheme-handler/https"
-              "x-scheme-handler/http"
-              "application/xhtml+xml"
-              "application/json"
-              "text/plain"
-              "text/html"
-            ]
-        );
-      in
-      {
-        associations.added = associations;
-        defaultApplications = associations;
-      };
-
   };
 }
