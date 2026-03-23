c4ffa5ef881c36b66854fa24ece62c03f841362e
Axioms <axiom@axioms.dev>
2026-03-23 01:42:14 -0400

    followup fix

--- modules/extra/syncthing/devices/Aries.nix
+++ modules/extra/syncthing/devices/Aries.nix
@@ -4,7 +4,7 @@
 }:
 
 {
-  home-manager.users."${config.syncthing.username}".services.syncthing.settings.devices.Acquarius = {
+  home-manager.users."${config.syncthing.username}".services.syncthing.settings.devices.Aries = {
     id = "IQSEQKD-3GNY7VT-4GQEWEM-XK247KD-WSDGWKV-LL7JB6O-OGE3Y7X-FEUYOQC";
   };
 }
