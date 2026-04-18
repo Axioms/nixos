5f8d0b4fcfe672e15f4c98cdaa81c6b881fb66b7
Axioms <atomic.axioms@gmail.com>
2026-04-18 01:30:29 -0400

    fix cachy kernel not compiling

--- modules/kernel/cachy.nix
+++ modules/kernel/cachy.nix
@@ -1,4 +1,10 @@
-{ inputs, pkgs, ... }:
+{
+  inputs,
+  pkgs,
+  lib,
+  config,
+  ...
+}:
 
 {
   nix.settings = {
@@ -13,4 +19,25 @@
   };
   nixpkgs.overlays = [ inputs.cachyos-kernel.overlays.pinned ];
   boot.kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-latest;
+
+  # TODO Remove when linuz 7.0 is on nix?
+  boot.initrd.luks.cryptoModules =
+    let
+      isModern = lib.versionAtLeast config.boot.kernelPackages.kernel.version "7.0";
+    in
+    lib.mkForce (
+      if isModern then
+        [
+          "aes"
+          "xts"
+          "sha256"
+        ]
+      else
+        [
+          "aes"
+          "aes_generic"
+          "xts"
+          "sha256"
+        ]
+    );
 }
