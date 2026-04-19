0bc5a5c0d5ae86602c642e6bf26582e632de8eb3
Axioms <axiom@axioms.dev>
2026-04-19 03:07:48 -0400

    add linux 7.0 cehck for notmal kernel

--- modules/kernel/normal.nix
+++ modules/kernel/normal.nix
@@ -1,5 +1,30 @@
-{ pkgs, ... }:
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}:
 
 {
+  # TODO Remove when linuz 7.0 is on nix?
+  boot.initrd.luks.cryptoModules =
+    let
+      isModern = lib.versionAtLeast config.boot.kernelPackages.kernel.version "7.0";
+    in
+    lib.mkForce (
+      if isModern then
+        [
+          "aes"
+          "xts"
+          "sha256"
+        ]
+      else
+        [
+          "aes"
+          "aes_generic"
+          "xts"
+          "sha256"
+        ]
+    );
   boot.kernelPackages = pkgs.linuxPackages_latest;
 }
