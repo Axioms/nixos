c680365a6440a8ccff127d27476a281066abf495
Axioms <axiom@axioms.dev>
2026-08-25 13:57:03 -0400

    fmt

--- modules/extra/development/zed.nix
+++ modules/extra/development/zed.nix
@@ -1,16 +1,14 @@
 { config, ... }:
 
 {
-  home-manager.users."${config.system.PrimaryUser}" =
-    { ... }:
-    {
-      programs.zed-editor = {
-        enable = true;
-        enableMcpIntegration = false;
-        mutableUserDebug = true;
-        mutableUserTasks = true;
-        mutableUserSettings = true;
-        mutableUserKeymaps = true;
-      };
+  home-manager.users."${config.system.PrimaryUser}" = _: {
+    programs.zed-editor = {
+      enable = true;
+      enableMcpIntegration = false;
+      mutableUserDebug = true;
+      mutableUserTasks = true;
+      mutableUserSettings = true;
+      mutableUserKeymaps = true;
     };
+  };
 }
