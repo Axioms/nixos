72d74d47989dce4289bf4bd6ddcfafe55cb9e6f2
Axioms <axiom@axioms.dev>
2026-09-08 12:59:23 -0400

    kernel updates

--- modules/kernel/cachy.nix
+++ modules/kernel/cachy.nix
@@ -17,18 +17,5 @@
   };
   nixpkgs.overlays = [ inputs.cachyos-kernel.overlays.pinned ];
   #TODO: remove extend once ddcci driver is fixed
-  boot.kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-latest.extend (
-    _final: prev: {
-      ddcci-driver = prev.ddcci-driver.overrideAttrs (oldAttrs: {
-        patches = [
-          (pkgs.fetchpatch {
-            name = "Use-sysfs_emit-and-field-width-specifier.patch";
-            url = "https://gitlab.com/liquidnya/ddcci-driver-linux/-/commit/9510aa4aebf32678884f55ae251e54012a354ed1.patch";
-            hash = "sha256-s12ers7nPFaHOB+8/S8t3dtdoR6slukkfNPdghgftNs=";
-          })
-        ]
-        ++ (oldAttrs.patches or [ ]);
-      });
-    }
-  );
+  boot.kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-latest;
 }
